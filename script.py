9774099780d955ba19b7a75f966f6c90dfbab188
elinmerlin <elinjar10@gmail.com>
2022-11-16 13:09:56 +0300

    gc-content func added

--- script.py
+++ script.py
@@ -37,5 +37,29 @@ def convert_rna_to_protein(rna: str) -> str:
             start += CODON_LENGTH
             end += CODON_LENGTH
     return polypeptide
+<<<<<<< HEAD
 
 
+def plot_the_gc_ratio(sequence: str, step=100) -> None:
+    """ Function that plots the GC-content graph """
+
+    ratios = []
+    start, end = 0, step
+    while end <= len(sequence):
+        bases_number = {'A': 0, 'T': 0, 'G': 0, 'C': 0}
+        for base in sequence[start:end]:
+            bases_number[base] += 1
+        ratio = ((bases_number['G'] + bases_number['C']) / step) * 100
+        ratios.append(ratio)
+        start += step
+        end += step
+    ratios.append(ratios[-1])
+    x = np.arange(0, len(sequence), step)
+    plt.step(x, ratios, where='post')
+    plt.suptitle('GC content distribution')
+    plt.ylabel('guanine-cytosine ratio (%)')
+    plt.xlabel('DNA bases position')
+    plt.savefig('gc_ratio_of_genome.png')
+
+=======
+>>>>>>> 8d4203f8b40639e1e2b7d202130f9267558058ed
